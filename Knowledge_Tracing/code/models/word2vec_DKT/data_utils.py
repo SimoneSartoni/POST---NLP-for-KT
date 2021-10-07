7944a391ea54e11e1ca0ee69f822f8a9caf9f7be
SimoneSartoni <10583763@polimi.it>
2021-10-07 19:03:56 +0200

    Attempt to make directory work on Kaggle

--- Knowledge_Tracing/code/models/word2vec_DKT/data_utils.py
+++ Knowledge_Tracing/code/models/word2vec_DKT/data_utils.py
@@ -46,7 +46,7 @@ def generate_encodings(df, encode_model):
     return inputs_generator, outputs_generator
 
 
-def load_dataset_NLP_skills(fn, batch_size=32, shuffle=True, repository=""):
+def load_dataset_NLP_skills(fn, batch_size=32, shuffle=True, repository="", keyedvectors=""):
     df = pd.read_csv(fn, encoding="ISO-8859-1")
 
     if "skill_id" not in df.columns:
@@ -74,7 +74,7 @@ def load_dataset_NLP_skills(fn, batch_size=32, shuffle=True, repository=""):
     # Step 3.1 - Generate NLP extracted encoding for problems
     loaded_dataset = dt(name="assistments_2009", path=repository, prefix="clean_datasets/")
     loaded_dataset.load_saved_texts()
-    encode_model = pretrained_word2vec(load=False)
+    encode_model = pretrained_word2vec(load=True, keyedvectors=keyedvectors)
     encode_model.fit()
     encode_model.encode_problems(loaded_dataset.problem_id_to_index, loaded_dataset.texts_list)
 
