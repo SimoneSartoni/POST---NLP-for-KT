1e260469bf005ff83494372bc296e9cdf6c55a8b
SimoneSartoni <10583763@polimi.it>
2021-10-13 13:30:16 +0200

    Attempt to make SAINT executable on Kaggle

--- Knowledge_Tracing/code/models/complex_models/config.py
+++ Knowledge_Tracing/code/models/complex_models/config.py
@@ -3,7 +3,7 @@ MAX_SEQ = 100
 EMBED_DIMS = 128
 ENC_HEADS = DEC_HEADS = 8
 NUM_ENCODER = NUM_DECODER = 6
-BATCH_SIZE = 16
+BATCH_SIZE = 128
 TRAIN_FILE = "/kaggle/input/assistmentds-2012/2012-2013-data-with-predictions-4-final.csv"  # Dataset path
 TOTAL_EXE = 16566
 TOTAL_CAT = 270
