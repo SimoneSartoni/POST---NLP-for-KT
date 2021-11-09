be0a221fcd5c3c48a87853691f00c003022d5085
SimoneSartoni <10583763@polimi.it>
2021-11-09 23:17:59 +0100

    Update data_utils.py

--- Knowledge_Tracing/code/models/DKT_models/complete_DKT/data_utils.py
+++ Knowledge_Tracing/code/models/DKT_models/complete_DKT/data_utils.py
@@ -201,6 +201,7 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
         input_shapes = [[None, encode_model.vector_size] for encode_model in encode_models]
         if encodings_kwargs['use_skills']:
             input_shapes.append([None])
+        input_shapes = tuple(input_shapes)
         output_shapes = [None]
         shapes = (input_shapes, output_shapes)
         print(types)
