dd769757bc9f1920fbebb5d0147b6a13a842feda
SimoneSartoni <10583763@polimi.it>
2021-11-09 19:02:49 +0100

    Fix gensim_pretrained_word2vec.py

--- Knowledge_Tracing/code/models/encoding_models/gensim_model/gensim_pretrained_word2vec.py
+++ Knowledge_Tracing/code/models/encoding_models/gensim_model/gensim_pretrained_word2vec.py
@@ -123,9 +123,11 @@ class pretrained_word2vec(base_model):
 
     def get_encoding(self, problem, norm=False):
         row = self.texts_df.loc[self.texts_df['problem_id'] == problem]
+        print(row)
         sentence_encoding = np.zeros(shape=self.vector_size)
         num = 0
         for word in row['body'].values:
+            print(word)
             if word in self.wordvectors.key_to_index:
                 sentence_encoding = sentence_encoding + np.array(self.wordvectors.get_vector(word, norm=norm))
                 num += 1
