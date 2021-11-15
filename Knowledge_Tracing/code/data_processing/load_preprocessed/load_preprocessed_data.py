e5bc397e98f1b924bb6b263cc2aab17f2e8edc66
SimoneSartoni <10583763@polimi.it>
2021-11-15 18:50:23 +0100

    Change management of preprocessing and load of datasets

--- Knowledge_Tracing/code/data_processing/load_preprocessed/load_preprocessed_data.py
+++ Knowledge_Tracing/code/data_processing/load_preprocessed/load_preprocessed_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from Knowledge_Tracing.code.utils.utils import try_parsing_date
 from Knowledge_Tracing.code.data_processing.get_assistments_texts import get_assistments_texts
-
+from ast import literal_eval
 
 def load_preprocessed_interactions(interactions_filepath=""):
     dtypes = {'user_id': 'int32', 'problem_id': 'int64',
@@ -23,5 +23,6 @@ def load_preprocessed_texts(texts_filepath=""):
     dtypes = {'problem_id': 'int64', 'body': "string", 'question_id': "int64"}
     print("loading csv.....")
     texts_df = pd.read_csv(texts_filepath, dtype=dtypes)
+    texts_df['body'] = texts_df['body'].apply(lambda x: literal_eval(x))
     print("shape of dataframe :", texts_df.shape)
     return texts_df
--- Knowledge_Tracing/code/data_processing/preprocess/process_data_assistments_2012.py
+++ Knowledge_Tracing/code/data_processing/preprocess/process_data_assistments_2012.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime
 
 import numpy as np
