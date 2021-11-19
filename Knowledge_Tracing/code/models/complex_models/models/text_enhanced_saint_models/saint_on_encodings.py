2ceb0164ecc17c187d644c877f538e1bc81aef79
SimoneSartoni <10583763@polimi.it>
2021-11-19 17:52:10 +0100

    FIx dataset in SAINT models

--- Knowledge_Tracing/code/models/complex_models/models/text_enhanced_saint_models/saint_on_encodings.py
+++ Knowledge_Tracing/code/models/complex_models/models/text_enhanced_saint_models/saint_on_encodings.py
@@ -14,7 +14,7 @@ class saint_on_encodings(nn.Module):
         self.n_decoder = n_decoder
         self.encoder = get_clones(EncoderBlock(enc_heads, n_dims, nb_questions, encoding_size, nb_skills, seq_len),
                                   n_encoder)
-        self.decoder = get_clones(DecoderBlock(dec_heads, n_dims, nb_responses, seq_len), n_decoder)
+        self.decoder = get_clones(DecoderBlock(dec_heads, n_dims, nb_responses, encoding_size, seq_len), n_decoder)
         self.fc = nn.Linear(2 * n_dims, 1)
 
     def forward(self, inputs, decoder_targets):
