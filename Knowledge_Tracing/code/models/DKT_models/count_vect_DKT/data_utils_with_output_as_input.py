ab4c111ce1dfe4d238ed094f630db46844ff39de
SimoneSartoni <10583763@polimi.it>
2021-11-22 11:42:46 +0100

    Fix count_vect_DKT_with_output_as_input

--- Knowledge_Tracing/code/models/DKT_models/count_vect_DKT/data_utils_with_output_as_input.py
+++ Knowledge_Tracing/code/models/DKT_models/count_vect_DKT/data_utils_with_output_as_input.py
@@ -8,6 +8,8 @@ from Knowledge_Tracing.code.models.encoding_models.count_vectorizer import count
 from Knowledge_Tracing.code.models.encoding_models.count_vectorizer import count_vectorizer
 from Knowledge_Tracing.code.data_processing.load_preprocessed.load_preprocessed_data import load_preprocessed_texts, \
     load_preprocessed_interactions
+from Knowledge_Tracing.code.data_processing.preprocess.group_interactions_by_user_id import generate_sequences_of_same_length
+
 MASK_VALUE = -1.0  # The masking value cannot be zero.
 
 
@@ -15,13 +17,14 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
                  interactions_filepath="../input/assistmentds-2012/2012-2013-data-with-predictions-4-final"
                                        ".csv",
                  save_filepath='/kaggle/working/', texts_filepath='../input/', min_df=2, max_df=1.0,
-                 min_questions=2, max_features=1000, max_questions=25, n_rows=None, n_texts=None,
-                 personal_cleaning=True):
+                 max_features=1000, interaction_sequence_len=25):
     df = load_preprocessed_interactions(interactions_filepath=interactions_filepath)
+    group = generate_sequences_of_same_length(df, seq_len=interaction_sequence_len, output_filepath='/kaggle/working')
+    del df
+    gc.collect()
     text_df = load_preprocessed_texts(texts_filepath=texts_filepath)
-    print(df)
-    df = df[['question_id', 'user_id', 'problem_id', 'correct']]
-    print(df)
+    print(group)
+    group = group[["user_id", "question_id", "problem_id", "correct", "elapsed_time", "skill"]]
     # Step 3.1 - Generate NLP extracted encoding for problems
     encode_model = count_vectorizer(min_df=min_df, max_df=max_df, binary=False, max_features=max_features)
     encode_model.fit(text_df, save_filepath)
@@ -31,12 +34,14 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
     gc.collect()
     print("number of words is: " + str(max_value))
     users = df['user_id'].unique()
-    train_users, test_users = train_test_split(users, test_size=0.2)
-    train_users, val_users = train_test_split(train_users, test_size=0.2)
+    print("splitting")
+    train, test = train_test_split(group, test_size=0.2)
+    train, val = train_test_split(train, test_size=0.2)
+    print("train size: ", train.shape, "validation size: ", val.shape)
 
 
     def generate_encodings_val():
-        for name, group in df.loc[df['user_id'].isin(val_users)].groupby('user_id'):
+        for name, group in val:
             document_to_term = []
             labels = np.array([], dtype=np.int)
             for problem, label in list(zip(group['question_id'].values, group['correct'].values)):
@@ -54,7 +59,7 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
             yield inputs, outputs
 
     def generate_encodings_test():
-        for name, group in df.loc[df['user_id'].isin(test_users)].groupby('user_id'):
+        for name, group in test:
             document_to_term = []
             labels = np.array([], dtype=np.int)
             for problem, label in list(zip(group['question_id'].values, group['correct'].values)):
@@ -71,7 +76,7 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
             outputs = o_label
             yield inputs, outputs
     def generate_encodings_train():
-        for name, group in df.loc[df['user_id'].isin(train_users)].groupby('user_id'):
+        for name, group in train:
             document_to_term = []
             labels = np.array([], dtype=np.int)
             for problem, label in list(zip(group['question_id'].values, group['correct'].values)):
@@ -89,7 +94,7 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
             yield inputs, outputs
     encoding_depth = encode_model.vector_size
 
-    def create_dataset(generate_encodings, users, encoding_depth):
+    def create_dataset(generate_encodings, dataset, encoding_depth):
         # Step 5 - Get Tensorflow Dataset
         types = ((tf.float32, tf.float32, tf.float32),
                  tf.float32)
@@ -102,7 +107,7 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
             output_shapes=shapes
         )
 
-        nb_users = len(users)
+        nb_users = len(dataset.values)
         if shuffle:
             dataset = dataset.shuffle(buffer_size=nb_users, reshuffle_each_iteration=True)
 
@@ -129,27 +134,11 @@ def load_dataset(batch_size=32, shuffle=True, dataset_name='assistment_2012',
         print(dataset)
         return dataset
 
-    train_set = create_dataset(generate_encodings_train, train_users, encoding_depth)
-    val_set = create_dataset(generate_encodings_val, val_users, encoding_depth)
-    test_set = create_dataset(generate_encodings_test, test_users, encoding_depth)
+    train_set = create_dataset(generate_encodings_train, train, encoding_depth)
+    val_set = create_dataset(generate_encodings_val, val, encoding_depth)
+    test_set = create_dataset(generate_encodings_test, test, encoding_depth)
 
     return train_set, val_set, test_set, encoding_depth
-
-
-def split_dataset(generator, total_size, test_fraction, val_fraction=None):
-    if not 0 < test_fraction < 1:
-        raise ValueError("test_fraction must be between (0, 1)")
-
-    if val_fraction is not None and not 0 < val_fraction < 1:
-        raise ValueError("val_fraction must be between (0, 1)")
-
-    train_set, test_set = train_test_split(generator, test_size=test_fraction)
-
-    val_set = None
-    if val_fraction:
-        train_set, val_set = train_test_split(train_set, test_size=val_fraction)
-
-    return train_set, test_set, val_set
 
 
 def get_target(y_true, y_pred, nb_encodings=300):
