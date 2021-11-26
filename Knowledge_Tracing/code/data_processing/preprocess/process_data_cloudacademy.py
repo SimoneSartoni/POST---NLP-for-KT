87b8eaf44236ceece13beae46ba829c3cba7ce69
SimoneSartoni <10583763@polimi.it>
2021-11-26 12:48:46 +0100

    fix preprocessing for CloudAcademy dataset

--- Knowledge_Tracing/code/data_processing/preprocess/process_data_cloudacademy.py
+++ Knowledge_Tracing/code/data_processing/preprocess/process_data_cloudacademy.py
@@ -76,7 +76,8 @@ def process_data_cloudacademy(min_questions=2, max_questions=50, interactions_fi
     texts_df = texts_df.loc[texts_df['problem_id'].isin(train_df['problem_id'])]
 
     train_df['answer'] = [1.0 if answer == 't' else 0.0 for answer in train_df['answer']]
-
+    print(train_df['answer'].mean())
+    print(train_df.describe())
     n_ids = len(questions_ids)
     # n_skills = len(train_df['skill'].unique())
     print("no. of problems :", n_ids)
