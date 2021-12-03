59f9300c2f4ae77311128c61af6078562188da44
SimoneSartoni <10583763@polimi.it>
2021-12-03 14:58:29 +0100

    Add print on dkt models

--- Knowledge_Tracing/code/models/DKT_models/DKT/version_1_on_skill/data_utils.py
+++ Knowledge_Tracing/code/models/DKT_models/DKT/version_1_on_skill/data_utils.py
@@ -98,12 +98,12 @@ def load_dataset(batch_size=32, shuffle=True,
 
 
 def get_target(y_true, y_pred, nb_encodings=300):
-    mask = 1 - tf.cast(tf.equal(y_true, MASK_VALUE), y_true.dtype)
-    count = tf.reduce_sum(tf.where(y_pred >= 0.5, 1.0, 0.0)) / tf.reduce_sum(tf.where(y_pred >= 0.0, 1.0, 0.0))
+    mask = 1. - tf.cast(tf.equal(y_true, MASK_VALUE), y_true.dtype)
+    count = tf.reduce_sum(tf.where(y_pred >= 0.0, y_pred, 0.0)) / tf.reduce_sum(tf.where(y_pred >= 0.0, 1.0, 0.0))
     tf.print(count, output_stream=sys.stdout)
     y_true = y_true * mask
     y_pred = y_pred * mask
-    count = tf.reduce_sum(tf.where(y_pred >= 0.5, 1.0, 0.0)) / tf.reduce_sum(tf.where(y_pred > 0.0, 1.0, 0.0))
+    count = tf.reduce_sum(tf.where(y_pred >= 0.0, y_pred, 0.0)) / tf.reduce_sum(tf.where(y_pred >= 0.0, 1.0, 0.0))
     tf.print(count, output_stream=sys.stdout)
 
     return y_true, y_pred
