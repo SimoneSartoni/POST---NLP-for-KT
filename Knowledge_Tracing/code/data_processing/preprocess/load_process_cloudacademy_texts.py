8f04ea8e122a1572f6b033f42cf0d9f218982c10
SimoneSartoni <10583763@polimi.it>
2021-12-22 11:54:32 +0100

    Fix load_process_cloudacademy_texts.py

--- Knowledge_Tracing/code/data_processing/preprocess/load_process_cloudacademy_texts.py
+++ Knowledge_Tracing/code/data_processing/preprocess/load_process_cloudacademy_texts.py
@@ -128,14 +128,10 @@ def get_cloudacademy_texts(personal_cleaning=True, texts_filepath='../input/', n
     # Using the preprocessing function to preprocess the tweet data
     renaming_dict = {"id": "problem_id", "description": "body"}
     df = df.rename(columns=renaming_dict, errors="raise")
-    df['unprocessed_text'] = df['body']
-    print("df after hunspell")
-    dictionary_US = hunspell.HunSpell('/usr/share/hunspell/en_US.dic', '/usr/share/hunspell/en_US.aff', format="html")
-    df['rapid'] = df['body'].apply(lambda text: dictionary_US.spell(text))
-    print(df)
     preprocess_data(df, 'body')
     print("df after preprocess data:")
     print(df)
+    df['plain_text'] = df['body']
     # Using tokenizer and removing the stopwords
     rem_stopwords_tokenize(df, 'body', personal_cleaning)
     print("df after stopwords tokenize:")
