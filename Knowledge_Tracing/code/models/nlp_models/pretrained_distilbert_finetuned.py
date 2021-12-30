83b68e16a0e5d078f745df79033029790007d069
SimoneSartoni <10583763@polimi.it>
2021-12-30 10:55:13 +0100

    Fix pretrained_distilbert_finetuned.py

--- Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert_finetuned.py
+++ Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert_finetuned.py
@@ -62,8 +62,8 @@ class SentenceSimilarityDataset(Dataset):
         anchor_mask = torch.from_numpy(np.array(anchor_mask))
         positive_ids = torch.from_numpy(np.concatenate(positive_ids))
         positive_mask = torch.from_numpy(np.array(positive_mask))
-        print(anchor_ids)
-        print(anchor_mask)
+        print(anchor_ids.size())
+        print(anchor_mask.size())
         inputs = {"anchor_ids": anchor_ids, "anchor_mask": anchor_mask,
                   "positive_ids": positive_ids, "positive_mask": positive_mask}
         return inputs
