635c947b04dcb1c38cae1369faab0260c1a4bdfe
SimoneSartoni <10583763@polimi.it>
2022-01-06 14:04:24 +0100

    Fix pretrained_distilbert.py

--- Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert.py
+++ Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert.py
@@ -277,7 +277,7 @@ class PretrainedDistilBERT():
             inputs = self.tokenizer(list(self.texts_df[text_column].values)[start:end], truncation=True,
                                     return_tensors="pt",
                                     padding=True)
-            ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
+            ids, attention_mask = inputs['input_ids'].cuda(), inputs['attention_mask'].cuda()
             output = self.model(input_ids=ids, attention_mask=attention_mask, output_attentions=False)
             encoding = output.to_tuple()[0]
             for problem_id, enc, attention in list(zip(self.texts_df['problem_id'].values[start:end],
