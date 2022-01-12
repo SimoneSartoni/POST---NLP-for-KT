832a679ea0257f444414616eaa9702311fd4d4c3
SimoneSartoni <10583763@polimi.it>
2022-01-12 15:36:38 +0100

    fix metrics.py

--- Knowledge_Tracing/code/evaluation/tensorflow/metrics.py
+++ Knowledge_Tracing/code/evaluation/tensorflow/metrics.py
@@ -104,9 +104,9 @@ class ColdStartBinaryAccuracy(tf.keras.metrics.BinaryAccuracy):
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         print("y_true")
-        print(y_true)
-        print(y_pred)
-        print(sample_weight)
+        print(tf.shape(y_true))
+        print(tf.shape(y_pred))
+        print(tf.shape(sample_weight))
         if tf.shape(y_true)[1] > self.window_size:
             print("true")
             y_true, y_pred = y_true[:, 0:self.window_size], y_pred[:, 0:self.window_size]
