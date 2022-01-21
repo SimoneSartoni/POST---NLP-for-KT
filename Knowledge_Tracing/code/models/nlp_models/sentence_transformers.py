c7384e2bbfd0d7ce74f93c799151015bae824c4d
SimoneSartoni <10583763@polimi.it>
2022-01-21 10:32:14 +0100

    fix sentence_transformers.py

--- Knowledge_Tracing/code/models/nlp_models/sentence_transformers.py
+++ Knowledge_Tracing/code/models/nlp_models/sentence_transformers.py
@@ -82,13 +82,13 @@ class sentence_transformer:
         self.texts_df[text_column].fillna("na", inplace=True)
         length = len(self.texts_df[text_column].values)
         embeddings = self.st_model.encode(sentences=self.texts_df[text_column].values[0:length//2], show_progress_bar=True)
-        for problem_id, embedding in list(zip(list(self.texts_df['problem_id'].values), embeddings)):
+        for problem_id, embedding in list(zip(list(self.texts_df['problem_id'].values[0:length//2]), embeddings)):
             self.embeddings[problem_id] = embedding
         del embeddings
         gc.collect()
         embeddings = self.st_model.encode(sentences=self.texts_df[text_column].values[length // 2:],
                                           show_progress_bar=True)
-        for problem_id, embedding in list(zip(list(self.texts_df['problem_id'].values), embeddings)):
+        for problem_id, embedding in list(zip(list(self.texts_df['problem_id'].values[length // 2:]), embeddings)):
             self.embeddings[problem_id] = embedding
         del embeddings
         gc.collect()
