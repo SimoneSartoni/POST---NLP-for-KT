19c02c2ab543c74c4fddcbf35cbbb1f5572ca42d
SimoneSartoni <10583763@polimi.it>
2022-01-31 19:35:54 +0100

    fix transform() in pretrained_distilbert.py

--- Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert.py
+++ Knowledge_Tracing/code/models/nlp_models/pretrained_distilbert.py
@@ -283,7 +283,8 @@ class PretrainedDistilBERT():
                                                        encoding, attention_mask)):
                 x = mean_pool(enc, attention, dim=0)
                 y = F.normalize(x, p=2, dim=0)
-                self.encodings[problem_id] = y.detach().cpu().numpy()
+                z = y.detach().cpu()
+                self.encodings[problem_id] = z.numpy()
             start = start + batch_size
             del [ids, attention_mask, output, encoding]
             gc.collect()
