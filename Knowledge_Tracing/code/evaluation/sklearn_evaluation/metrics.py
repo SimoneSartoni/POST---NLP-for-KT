c03d311830dca29674e8fac7156aa6da97c19a57
SimoneSartoni <10583763@polimi.it>
2022-02-21 13:43:25 +0100

    fix

--- Knowledge_Tracing/code/evaluation/sklearn_evaluation/metrics.py
+++ Knowledge_Tracing/code/evaluation/sklearn_evaluation/metrics.py
@@ -49,4 +49,4 @@ def compute_metrics(y_true, y_pred):
     acc_50, auc_50 = cold_start_binary_accuracy(y_true, y_pred, 50), cold_start_auc(y_true, y_pred, 50)
     prec, rec = precision(y_true, y_pred), recall(y_true, y_pred)
     tp, tn, fp, fn = confusion_matrix_score(y_true, y_pred)
-    return acc, auc, acc_10, auc_10, acc_30, auc_30, acc_50, auc_50, tp, tn, fp, fn
+    return acc, auc, acc_10, auc_10, acc_30, auc_30, acc_50, auc_50, prec, rec, tp, tn, fp, fn
