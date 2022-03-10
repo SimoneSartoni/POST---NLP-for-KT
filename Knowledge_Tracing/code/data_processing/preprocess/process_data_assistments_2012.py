37ffcd8ac9b31b0b872ddc8349dd3da6ce959ea9
SimoneSartoni <10583763@polimi.it>
2022-03-10 14:55:49 +0100

    add print on process_data_assistments_2012.py

--- Knowledge_Tracing/code/data_processing/preprocess/process_data_assistments_2012.py
+++ Knowledge_Tracing/code/data_processing/preprocess/process_data_assistments_2012.py
@@ -23,6 +23,14 @@ def process_data_assistments_2012(min_questions=2, max_questions=50, interaction
         train_df = pd.read_csv(interactions_filepath, dtype=dtypes)
     print("shape of dataframe :", train_df.shape)
 
+    questions_ids = train_df['problem_id'].unique()
+    n_ids = len(questions_ids)
+    n_skills = len(train_df['skill_id'].unique())
+    print("no. of problems :", n_ids)
+    print("no. of skills: ", n_skills)
+    print("shape after exclusion:", train_df.shape)
+    print("number of users:" + str(len(train_df['user_id'].unique())))
+
     # Step 3.1 - Define start, end and elapsed time, fill no timed elapsed time and cap values under a max
     train_df['start_time'] = [try_parsing_date(x) for x in train_df['start_time']]
     train_df['end_time'] = [try_parsing_date(x) for x in train_df['end_time']]
@@ -59,6 +67,7 @@ def process_data_assistments_2012(min_questions=2, max_questions=50, interaction
     train_df['skill'], _ = pd.factorize(train_df['skill'], sort=True)
     print("shape after factorize:", train_df.shape)
 
+
     # Step 5 - Compute number of unique skills ids and number of unique question ids
     questions_ids = train_df['problem_id'].unique()
     n_ids = len(questions_ids)
@@ -66,19 +75,20 @@ def process_data_assistments_2012(min_questions=2, max_questions=50, interaction
     print("no. of problems :", n_ids)
     print("no. of skills: ", n_skills)
     print("shape after exclusion:", train_df.shape)
-
-    print("Get texts, intersection...")
+    print("number of users:" + str(len(train_df['user_id'].unique())))
 
     # Step 6 - Remove questions interactions we do not have text
     texts_df = load_process_assistments_texts(personal_cleaning=personal_cleaning, texts_filepath=texts_filepath, n_texts=n_texts, make_sentences_flag=make_sentences_flag)
     train_df = train_df.loc[train_df['problem_id'].isin(texts_df['problem_id'])]
     texts_df = texts_df.loc[texts_df['problem_id'].isin(train_df['problem_id'])]
 
+    questions_ids = train_df['problem_id'].unique()
     n_ids = len(questions_ids)
     n_skills = len(train_df['skill'].unique())
     print("no. of problems :", n_ids)
     print("no. of skills: ", n_skills)
     print("shape after exclusion:", train_df.shape)
+    print("number of users:" + str(len(train_df['user_id'].unique())))
     train_df['skill'], _ = pd.factorize(train_df['skill'], sort=True)
     texts_df['question_id'], _ = pd.factorize(texts_df['problem_id'], sort=True)
     train_df['question_id'], _ = pd.factorize(train_df['problem_id'], sort=True)
