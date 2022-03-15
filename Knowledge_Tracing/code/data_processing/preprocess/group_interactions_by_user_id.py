a5cb64ec9c567b33b7614ba26c947d32d71b1b5c
SimoneSartoni <10583763@polimi.it>
2022-03-15 14:51:03 +0100

    add print in group_interactions_by_user_id.py

--- Knowledge_Tracing/code/data_processing/preprocess/group_interactions_by_user_id.py
+++ Knowledge_Tracing/code/data_processing/preprocess/group_interactions_by_user_id.py
@@ -10,9 +10,14 @@ def generate_sequences_of_same_length(df, seq_len, min_seq_len, output_filepath=
     correct = []
     start_time, end_time, elapsed_time = [], [], []
     timestamp = []
+    chunk_count = 0
+    n_seq_longer_than_seq_len = 0
     for name, group in grouped:
         index = 0
         while index + seq_len < len(group['question_id'].values):
+            if index == 0:
+                n_seq_longer_than_seq_len += 1
+            chunk_count += 1
             user_id.append(name)
             problem_id.append(group['problem_id'].values[index:index+seq_len])
             question_id.append(group['question_id'].values[index:index+seq_len])
@@ -33,6 +38,9 @@ def generate_sequences_of_same_length(df, seq_len, min_seq_len, output_filepath=
             end_time.append(group['end_time'].values[index:])
             elapsed_time.append(group['elapsed_time'].values[index:])
             timestamp.append(group['timestamp'].values[index:])
+
+    print("Number of users with sequences longer than "+str(seq_len)+" is: ", +str(n_seq_longer_than_seq_len))
+    print("Number of times we chunk sequences with seq_len="+str(seq_len)+" is: ", +str(chunk_count))
     data = {'question_id': question_id, 'problem_id': problem_id, 'user_id': user_id, 'correct': correct,
             'skill': skill, 'start_time': start_time, 'end_time': end_time, 'elapsed_time': elapsed_time,
             'timestamp': timestamp}
